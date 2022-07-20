63b59e7c956e8a153d1e903254b2202f17248bc9
hyperboria <anywayaura@gmail.com>
2022-07-20 15:07:54 +0700

    clean requirements nothing commented in code readme markdown is okay, tbh

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def get_weather(place):
     params = {'nTqum':'', 'lang': 'ru'}
     result = requests.get(f'https://wttr.in/{place}', params=params)
     result.raise_for_status()
-    # if 'error' in result.json():
-    #     raise requests.exceptions.HTTPError(result.json()['error'])
     return result.text
 
 
