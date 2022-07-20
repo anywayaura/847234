6e00fb00d7129ae36ba01633f77348f3f9b0cfe3
hyperboria <anywayaura@gmail.com>
2022-07-20 12:51:43 +0700

    upgraded request mechanism prevented status errors added giga-scaling new fancy README.md file also requierements

---
+++ README.md
@@ -0,0 +1,8 @@
+Script checks weather for the places in the list.
+Just add needed places inside it.
+
+installation:
+pip install -r requirements.txt
+
+run:
+python main.py
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
 
 
+places = ['London', 'Аэропорт Шереметьево', 'Череповец']
+
 def get_weather(place):
-    result = requests.get(f'https://wttr.in/{place}?nTqum&lang=ru')
+    params = {'nTqum':'', 'lang': 'ru'}
+    result = requests.get(f'https://wttr.in/{place}', params=params)
+    result.raise_for_status()
+    # if 'error' in result.json():
+    #     raise requests.exceptions.HTTPError(result.json()['error'])
     return result.text
 
 
 if __name__ == '__main__':
-    print(get_weather('London'))
-    print(get_weather('Аэропорт Шереметьево'))
-    print(get_weather('Череповец'))
+    for place in places:
+        print(get_weather(place))
